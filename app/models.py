f3c612612b75acf055df169fc0488fe079158afd
Arki Valencia II <dev-admin@arkiproductions.me>
2016-08-10 18:20:06 +0800

    Rework: Models
    Reworked database relationships to work more seamlessly
    with Categories and Items

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from app import db
 class Category(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
 	name = db.Column(db.String(180), index = True, unique = True)
-	items = db.relationship('Item', back_populates = 'category') 
+	items = db.relationship('Item', backref = 'category') 
 
 	def __repr__(self):
 		return '<Category %r>' % self.name
@@ -14,7 +14,6 @@ class Item(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
 	name = db.Column(db.String(180), nullable = False, index = True)
 	category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
-	category = db.relationship('Category', back_populates = 'items')
 
 	def __repr__(self):
 		return '<Item %r>' % self.name
