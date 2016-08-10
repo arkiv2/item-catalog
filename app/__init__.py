d65667af5ac418c7e41ccbf876db2adbea8eb8f2
Arki Valencia II <dev-admin@arkiproductions.me>
2016-08-10 17:54:24 +0800

    Refactored init

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,8 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, instance_relative_config = True)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
-from app import views
-from app import models
+from app import views, models
