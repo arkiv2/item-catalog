977409d0fb99cc7c85d64dd6488da553437e7188
Arki Valencia II <dev-admin@arkiproductions.me>
2016-08-11 00:46:56 +0800

    Feat: Implemented jinja template rendering
    Modified views to render templates instead of strings

--- app/views.py
+++ app/views.py
@@ -1,7 +1,6 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 def index():
-	return "Welcome!"
-
-	# return render_template('filename.html')  // TODO
+	return render_template('index.html') 
