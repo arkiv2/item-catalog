e1884fac03bbd42d7ed9d5f785fe9c2381446dc1
Arki Valencia II <dev-admin@arkiproductions.me>
2016-08-10 17:15:31 +0800

    Added support for instance configurations

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config.from_object('Config')
+app = Flask(__name__, instance_relative_config = True)
+app.config.from_object('config.Config')
 db = SQLAlchemy(app)
 
 from app import views
