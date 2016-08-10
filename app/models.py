f5af89747c73e628646800fa19347dfa4a933488
Arki Valencia II <dev-admin@arkiproductions.me>
2016-08-10 18:00:37 +0800

    Bug Fix : models.py
    Fixed a bug where db_create can't create a database due to a typo in models.py

--- app/models.py
+++ app/models.py
@@ -1,11 +1,20 @@
 from app import db
 
+
 class Category(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
 	name = db.Column(db.String(180), index = True, unique = True)
-	items = db.relationship("Item", db.back_populates = "category") 
+	items = db.relationship('Item', back_populates = 'category') 
+
+	def __repr__(self):
+		return '<Category %r>' % self.name
+
+
 class Item(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
 	name = db.Column(db.String(180), nullable = False, index = True)
 	category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
-	category = db.relationship("Category", db.back_populates = "items")
+	category = db.relationship('Category', back_populates = 'items')
+
+	def __repr__(self):
+		return '<Item %r>' % self.name
