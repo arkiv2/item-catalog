032a5245df4689774812bc2646c07c9acc438993
Arki Valencia II <dev-admin@arkiproductions.me>
2016-08-11 12:50:47 +0800

    Updated Feat: Index Page
    Reworked views to render index page as an html with
    variables from database queries

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from app import app
 from flask import render_template
+from models import Category
 
 @app.route('/')
 def index():
-	return render_template('index.html') 
+	categories = Category.query.all()
+	return render_template('index.html', categories=categories) 
