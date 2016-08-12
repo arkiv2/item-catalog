b917e352aa70dac50a4fcd81ce3cab1243b96cf3
Arki Valencia II <dev-admin@arkiproductions.me>
2016-08-12 20:37:46 +0800

    Fix: Rolled back decorator functions to context processor
    Removed decorator functions in rendering sidebar template
    Added context processor to pass sidebar data in templates

--- app/views.py
+++ app/views.py
@@ -2,39 +2,32 @@ from app import app
 from flask import render_template
 from models import Category, Item
 
-def include_sidebar_data(fn):
-	template_name = fn()
+@app.context_processor
+def add_template_helpers():
 	category_sidebar = Category.query.all()
-	def wrapped():
-		return render_template(template_name, category_sidebar=category_sidebar)
-	return wrapped
+	return dict(category_sidebar=category_sidebar)
 
 @app.route('/')
-@include_sidebar_data
 def index():
 	items = Item.query.all()
 	return render_template('index.html', items=items) 
 
-@app.route('category')
-@include_sidebar_data
-def category():
+@app.route('/category')
+def category_index():
 	categories = Category.query.all()
 	return render_template('categories.html')
 
-@app.route('category/<category_name>')
-@include_sidebar_data
+@app.route('/category/<category_name>')
 def category(category_name):
 	category = Category.query.filter_by(slug_name=category_name).first_or_404()
 	return render_template('category')
 
-@app.route('item')
-@include_sidebar_data
-def item():
+@app.route('/item')
+def item_index():
 	items = Item.query.all()
 	return render_template('items.html', items=items)
 
-@app.route('item/<item_name>')
-@include_sidebar_data
+@app.route('/item/<item_name>')
 def item(item_name):
 	item = Item.query.filter_by(slug_name=item_name).first_or_404()
 	return render_template('item.html', )
